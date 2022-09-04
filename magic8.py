c6f00ff2c337c6a8815911de288415a8685c7555
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2022-09-04 14:28:19 -0500

    v1.0

--- magic8.py
+++ magic8.py
@@ -1,5 +1,31 @@
 import random
 
 name = "Andrew"
-question = "Will Buccaneers win the Super Bowl?"
+question = "Will the Buccaneers win the Super Bowl?"
 answer = ""
+randomNumber = random.randint(1, 9)
+
+print(name + " asks: " + question)
+
+if randomNumber == 1:
+  answer = "Yes, definitely."
+elif randomNumber == 2:
+  answer = "It is decidedly so."
+elif randomNumber == 3:
+  answer = "Without a doubt."
+elif randomNumber == 4:
+  answer = "Reply hazy, try again."
+elif randomNumber == 5:
+  answer = "Ask again later."
+elif randomNumber == 6:
+  answer = "Better not tell you now."
+elif randomNumber == 7:
+  answer = "My sources say no."
+elif randomNumber == 8:
+  answer = "Outlook not so good."
+elif answer == 9:
+  answer = "Very doubtful."
+else:
+  print("Woops. There's been an error.")
+
+print(answer)
